f288ddbc29ed058a655fad8727549b56464cdfd5
Nola103 newfield <nolanew2008@gmail.com>
2023-11-15 16:46:52 +0000

    circ

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -31,7 +31,7 @@ var init = function (window) {
 
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 100; i++) {
             drawCircle()
             }
             
@@ -46,23 +46,17 @@ var init = function (window) {
         */
        
             function update() {
-                // TODO 4 : Update the circle's position //
+                // TODO 4 / 8 : Update the circle's position //
               
               
-                physikz.updatePosition(circles[0]);
-                physikz.updatePosition(circles[1]);
-                physikz.updatePosition(circles[2]);
-                physikz.updatePosition(circles[3]);
-                physikz.updatePosition(circles[4]);
-             
+               for (var i = 0; i < circles.length; i++)
+               { physikz.updatePosition(circles[i]);}
               
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-              game.checkCirclePosition(circles[0]);
-              game.checkCirclePosition(circles[1]);
-              game.checkCirclePosition(circles[2]);
-              game.checkCirclePosition(circles[3]);
-              game.checkCirclePosition(circles[4]);
+            for (var i = 0; i < circles.length; i++)
+            {  game.checkCirclePosition(circles[i]);}
+             
             
             // TODO 9 : Iterate over the array
            
